92d0b27f669b63fcf6f233d1831f2f72e2263486
Shiv Kumar <shiv.kmr003@gmail.com>
2023-12-21 02:04:27 -0500

    Introduced Host Builder in order to correctly associate the ConfigurationRoot with the added/registered Loggers so that they pick up the Logging configuration from the appsettings.json file

--- ExceptionsHierarchy/Program.cs
+++ ExceptionsHierarchy/Program.cs
@@ -3,7 +3,10 @@ using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Options;
 
 namespace ExceptionsHierarchy;
 
@@ -11,31 +14,38 @@ internal static class Program
 {
     private static async Task Main()
     {
-        var channel = new InMemoryChannel();
-        ApplicationLogger applicationLogger = CreateApplicationLogger(channel, "MktInt");
+        using var telemetryChannel = new InMemoryChannel();
+        var (host, applicationLogger) = CreateHost(telemetryChannel, "MktInt");
+        await ExecuteApplication(telemetryChannel, applicationLogger);
+
+        await Console.Out.WriteLineAsync("Finished Executing Application");
+        await Console.Out.WriteLineAsync("Press CTRL + C to shut down...");
+        await host.RunAsync();
+    }
+
+    private static async Task ExecuteApplication(InMemoryChannel telemetryChannel, ApplicationLogger applicationLogger)
+    {
         try
         {
-            var someContextInfo = "Hello Context!";
+            var someContextInfo = $"Some super important Contextual information - {DateTimeOffset.UtcNow.ToLocalTime():o}";
 
-            var exception = new PayPlansValidationException(new ExceptionData("This is a test message", "RatedStateNotSupportedException"));
+            var exception = new RatedStateNotSupportedException(new ExceptionData("This is a test message", "RatedStateNotSupportedException"));
             exception.Data.Add("CorrelationId", Guid.NewGuid().ToString("N"));
             exception.Data.Add("InterviewId", Guid.NewGuid().ToString("N"));
             exception.Data.Add("SomeContextInfo", someContextInfo);
 
             throw exception;
-
-            /*
-            **** Doing Structured logging manually ****
-            logger.LogError(new EventId((int)LogEvent.PayPlanValidationFailed, LogEvent.PayPlanValidationFailed.ToString()), exception,
-                "Message: {Message}, Exception Category: {ExceptionCategory}, Reason: {Reason}, Method Name: {MethodName}, CorrelationId: {CorrelationId}, InterviewId: {InterviewId}, SomeContextInfo: {SomeContextInfo}",
-                exception.Message,
-                exception.ExceptionCategory,
-                exception.Reason,
-                nameof(Main),
-                Guid.NewGuid().ToString("N"),
-                Guid.NewGuid().ToString("N"),
-                someContextInfo);
-            */
+            
+            ////**** Doing Structured logging manually ****
+            ////logger.LogError(new EventId((int)LogEvent.PayPlanValidationFailed, LogEvent.PayPlanValidationFailed.ToString()), exception,
+            ////    "Message: {Message}, Exception Category: {ExceptionCategory}, Reason: {Reason}, Method Name: {MethodName}, CorrelationId: {CorrelationId}, InterviewId: {InterviewId}, SomeContextInfo: {SomeContextInfo}",
+            ////    exception.Message,
+            ////    exception.ExceptionCategory,
+            ////    exception.Reason,
+            ////    nameof(Main),
+            ////    Guid.NewGuid().ToString("N"),
+            ////    Guid.NewGuid().ToString("N"),
+            ////    someContextInfo);            
         }
         catch (InterviewBaseException e)
         {
@@ -43,38 +53,43 @@ internal static class Program
         }
         finally
         {
-            channel.Flush();
+            telemetryChannel.Flush();
             await Task.Delay(TimeSpan.FromMilliseconds(1000));
-            channel.Dispose();
+            telemetryChannel.Dispose();
         }
     }
 
-    private static ApplicationLogger CreateApplicationLogger(ITelemetryChannel channel, string roleName)
+    private static (IHost, ApplicationLogger) CreateHost(InMemoryChannel telemetryChannel, string roleName)
     {
-        IConfigurationRoot configuration = new ConfigurationBuilder()
+        IConfigurationRoot configurationRoot = default!;
+        var hostBuilder = Host.CreateDefaultBuilder();
+        var host = hostBuilder
+        .ConfigureHostConfiguration(configurationBuilder =>
+        {
+            configurationRoot = configurationBuilder
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .Build();
-
-        IServiceCollection services = new ServiceCollection();
-        services
-            .Configure<TelemetryConfiguration>(config => config.TelemetryChannel = channel)
+        })
+        .ConfigureServices(services =>
+        {
+            services
+            .Configure<TelemetryConfiguration>(config => config.TelemetryChannel = telemetryChannel)
             .AddLogging(builder =>
             {
                 builder
-                    .ClearProviders()
-                    .AddApplicationInsights(
-                        configureTelemetryConfiguration: (config) => config.ConnectionString = configuration["ApplicationInsights:ConnectionString"],
-                        configureApplicationInsightsLoggerOptions: (options) => { }
-            )
-            .AddJsonConsole(options =>
+                .AddApplicationInsights(
+                    configureTelemetryConfiguration: config => config.ConnectionString = configurationRoot["ApplicationInsights:ConnectionString"],
+                    configureApplicationInsightsLoggerOptions: options => { })
+                .AddJsonConsole(options =>
                 {
                     options.JsonWriterOptions = new System.Text.Json.JsonWriterOptions() { Indented = true };
                 });
             });
+        })
+        .Build();
 
-        IServiceProvider serviceProvider = services.BuildServiceProvider();
-        ILogger<DomainFacade> logger = serviceProvider.GetRequiredService<ILogger<DomainFacade>>();
-        return new ApplicationLogger(logger, roleName);
+        var logger = host.Services.GetRequiredService<ILogger<DomainFacade>>();
+        return (host, new ApplicationLogger(logger, roleName));
     }
 }
